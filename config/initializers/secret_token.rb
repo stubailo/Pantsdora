e100c25acdea58a5b2b4064f0e5089db716e566d
sashko <sashko@sashko-ubuntu.(none)>
2012-02-12 20:12:47 -0500

    all of rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pantsdora::Application.config.secret_token = 'cac7fa925a524b0be642e40093e893e8bcc29aec84a4f5db0bfd387bcdec7d12211078692781e08af6ca97de9d181820bd738f3b26937d3f6bc5fa8a4ad4eef8'
